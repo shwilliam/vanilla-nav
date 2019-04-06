fc1c2af2a8a77a83429d19c80ea2630d1468b7f3
William L <w-lindvall@outlook.com>
2019-04-05 23:23:31 -0700

    feat: close on outside click & esc

--- menu.js
+++ menu.js
@@ -8,6 +8,45 @@
       [l - 1]: menuItemLastEl
     } = menuListEl.children
 
+    const openMenu = () => {
+      menuToggleEl.checked = true
+      menuEl.setAttribute('aria-expanded', true)
+      menuEl.setAttribute('aria-hidden', false)
+
+      // lock scroll
+      const { scrollX, scrollY } = window
+      window.onscroll = () => window.scrollTo(scrollX, scrollY)
+
+      // enable tab navigation
+      for (
+        let i = 0, menuListLength = menuListEl.children.length;
+        i < menuListLength;
+        i++
+      ) {
+        const menuListItemEl = menuListEl.children[i]
+        menuListItemEl.children[0].setAttribute('tabindex', 0)
+      }
+    }
+
+    const closeMenu = () => {
+      menuToggleEl.checked = false
+      menuEl.setAttribute('aria-expanded', false)
+      menuEl.setAttribute('aria-hidden', true)
+
+      // enable scroll
+      window.onscroll = null
+
+      // disable tab navigation
+      for (
+        let i = 0, menuListLength = menuListEl.children.length;
+        i < menuListLength;
+        i++
+      ) {
+        const menuListItemEl = menuListEl.children[i]
+        menuListItemEl.children[0].setAttribute('tabindex', -1)
+      }
+    }
+
     // trap tab navigation within menu
     menuItemLastEl.addEventListener('keydown', (e) => {
       if (e.keyCode === 9 && !e.shiftKey) {
@@ -28,40 +67,17 @@
       const isOpening = target.checked
 
       if (isOpening) {
-        // open menu
-        menuEl.setAttribute('aria-expanded', true)
-        menuEl.setAttribute('aria-hidden', false)
-
-        // lock scroll
-        const { scrollX, scrollY } = window
-        window.onscroll = () => window.scrollTo(scrollX, scrollY)
-
-        // enable tab navigation
-        for (
-          let i = 0, menuListLength = menuListEl.children.length;
-          i < menuListLength;
-          i++
-        ) {
-          const menuListItemEl = menuListEl.children[i]
-          menuListItemEl.children[0].setAttribute('tabindex', 0)
-        }
+        openMenu()
       } else {
-        // close menu
-        menuEl.setAttribute('aria-expanded', false)
-        menuEl.setAttribute('aria-hidden', true)
+        closeMenu()
+      }
+    })
 
-        // enable scroll
-        window.onscroll = null
+    menuEl.addEventListener('click', closeMenu)
 
-        // disable tab navigation
-        for (
-          let i = 0, menuListLength = menuListEl.children.length;
-          i < menuListLength;
-          i++
-        ) {
-          const menuListItemEl = menuListEl.children[i]
-          menuListItemEl.children[0].setAttribute('tabindex', -1)
-        }
+    window.addEventListener('keydown', (e) => {
+      if (e.keyCode === 27) {
+        closeMenu()
       }
     })
   })
